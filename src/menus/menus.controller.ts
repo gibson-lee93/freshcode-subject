024a1bac72edd068825108cb67d9ace6f851536f
Gibson Lee <gb.lee.gibson@gmail.com>
2021-11-05 01:18:19 +0900

    feat: create menu
    - 메뉴 생성 API 추가
    - 메뉴 생성시 validation pipe(dto) 추가
    related issue: #9

--- src/menus/menus.controller.ts
+++ src/menus/menus.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateMenuDto } from './dto/create-menu.dto';
+import { Menu } from './entities/menu.entity';
 import { MenusService } from './menus.service';
 
 @Controller('menus')
 export class MenusController {
   constructor(private menusService: MenusService) {}
+
+  @Post()
+  createMenu(@Body() createMenuDto: CreateMenuDto): Promise<Menu> {
+    return this.menusService.createMenu(createMenuDto);
+  }
 }
--- src/menus/menus.repository.ts
+++ src/menus/menus.repository.ts
@@ -1,4 +1,24 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Menu } from 'src/menus/entities/menu.entity';
+import { CreateMenuDto } from './dto/create-menu.dto';
+import { InternalServerErrorException } from '@nestjs/common';
 @EntityRepository(Menu)
-export class MenusRepository extends Repository<Menu> {}
+export class MenusRepository extends Repository<Menu> {
+  async createMenu(createMenuDto: CreateMenuDto): Promise<Menu> {
+    const { name, description, isSold, badge } = createMenuDto;
+
+    const menu: Menu = this.create({
+      name,
+      description,
+      isSold,
+      badge,
+    });
+
+    try {
+      await this.save(menu);
+      return menu;
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+}
