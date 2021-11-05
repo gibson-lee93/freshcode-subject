31f8d85619af0a75cd0097389073a50fe2492a7c
lhj0621 <lhj0621@gmail.com>
2021-11-05 10:51:36 +0900

    feat: 카테고리 생성 - #22
    - 카테고리 생성  API 추가

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoriesRepository } from './categories.repository';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoriesService {
@@ -8,4 +10,8 @@ export class CategoriesService {
     @InjectRepository(CategoriesRepository)
     private categoriesRepository: CategoriesRepository,
   ) {}
+
+  createCategory(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    return this.categoriesRepository.createCategory(createCategoryDto);
+  }
 }
