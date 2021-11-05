aeab5b41079d57d80cf7305a80597de5151eab9d
doogler777 <doogler777@gmail.com>
2021-11-05 18:05:31 +0900

    feat:항목 수정 기능 추가

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { Item } from './entities/item.entity';
 import { ItemsService } from './items.service';
 import { MenusService } from 'src/menus/menus.service';
+import { UpdateItemDto } from './dto/update-item.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -16,5 +17,12 @@ export class ItemsController {
     // menuId로 menu 조회
     const menu = await this.menusService.getMenuById(createItemDto.menuId);
     return this.itemsService.createItem(createItemDto, menu);
+  }
+
+  @Patch('/')
+  async updateItem(@Body() updateItemDto: UpdateItemDto): Promise<Item> {
+    // menuId로 menu 조회
+    await this.menusService.getMenuById(updateItemDto.menuId);
+    return this.itemsService.updateItem(updateItemDto);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Menu } from 'src/menus/entities/menu.entity';
 import { CreateItemDto } from './dto/create-item.dto';
+import { UpdateItemDto } from './dto/update-item.dto';
 import { Item } from './entities/item.entity';
 import { ItemsRepository } from './items.repository';
 
@@ -14,5 +15,22 @@ export class ItemsService {
 
   createItem(createItemDto: CreateItemDto, menu: Menu): Promise<Item> {
     return this.itemsRepository.createItem(createItemDto, menu);
+  }
+
+  async updateItem(updateItemDto: UpdateItemDto): Promise<Item> {
+    const item: Item = await this.itemsRepository.findOne(updateItemDto.itemId);
+    const { name, size, price, isSold } = updateItemDto;
+
+    if (!item) {
+      throw new NotFoundException('유효한 항목 id가 아닙니다.');
+    }
+
+    item.name = name;
+    item.size = size;
+    item.price = price;
+    item.isSold = isSold;
+
+    await this.itemsRepository.save(item);
+    return item;
   }
 }
