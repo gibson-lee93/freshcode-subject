9fea4f0df65283a7a11437b30081e07a825fc6d5
Gibson Lee <gb.lee.gibson@gmail.com>
2021-12-06 23:20:01 +0900

    refactor: delete unnecessary file
    - delete local-auth.guard
    - delete local.strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,18 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './auth-guard/jwt-auth.guard';
-import { LocalAuthGuard } from './auth-guard/local-auth.guard';
 import { AuthService } from './auth.service';
 import { GetUser } from './get-user.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Post('logout')
   async logout(@GetUser() user) {
     await this.authService.logout(user);
