74bef28a54f2eb808af15cef6c921f01126e633f
Gibson Lee <gb.lee.gibson@gmail.com>
2021-12-09 22:50:44 +0900

    refactor: user module
    - return simple message when successfully created user
    - add error handling for not found user when findOne user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,13 @@
-import { IsEmail, IsEnum, IsString } from 'class-validator';
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { UserRole } from '../entities/user.entity';
 
 export class CreateUserDto {
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @IsString()
+  @IsNotEmpty()
   password: string;
 
   @IsEnum(UserRole)
